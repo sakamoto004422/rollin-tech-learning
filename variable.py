51df0060a90cac8766ce1c8a30994edb80340486
sakamoto004422 <k.sakamoto0203@gmail.com>
2018-02-05 00:58:20 +0900

    kansuu

--- variable.py
+++ variable.py
@@ -1,3 +1,15 @@
 a=100
 b=999
 print(a*b)
+
+def keisan(c,d,e):
+    if c+d+e >= 30:
+        print(e+d+c)
+        return "足した"
+    else:
+        print(a-b-c)
+        return "引いた"
+
+print(keisan(10,15,20))
+
+print("よ")
