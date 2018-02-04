d5a67303b88ba270798a30ebe27a8dcf949aa8d5
sakamoto004422 <k.sakamoto0203@gmail.com>
2018-02-05 01:08:09 +0900

    tuika

---
+++ calculation.py
@@ -0,0 +1,15 @@
+def keisan(c,d,e):
+    if c+d+e >= 30:
+        print(e+d+c)
+        return "足した"
+    else:
+        print(c-d-e)
+        return "引いた"
+
+print(keisan(10,15,20))
+
+print("よ")
+
+print(keisan(10,5,3))
+
+print("よ")
